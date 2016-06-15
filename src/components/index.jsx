95d3ac22052fdfcef1f66a3c9926e7545e6ad110
Weiguo <wxhh61@gmail.com>
2016-06-15 14:39:41 -0400

    add

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -58,7 +58,7 @@ const Home = () => (
     <Row className="show-grid">
     <Carousel>
     <Carousel.Item>
-    <img width={900} height={500} text-align="center"  alt="900x500" src= { require("./horse.jpg")} />
+    <img width={900} height={500} text-align="center"  alt="900x500" src= { require("../public/horse.jpg")} />
       <Carousel.Caption>
         <h3>马1的名字</h3>
         <p>马1的介绍</p>
@@ -66,7 +66,7 @@ const Home = () => (
     </Carousel.Item>
 
         <Carousel.Item>
-    <img width={900} height={500} text-align="center"  alt="900x500" src= { require("./horse.jpg")} />
+    <img width={900} height={500} text-align="center"  alt="900x500" src= { require("../public/horse.jpg")} />
       <Carousel.Caption>
         <h3>马2的名字</h3>
         <p>马2的介绍</p>
@@ -74,7 +74,7 @@ const Home = () => (
     </Carousel.Item>
 
        <Carousel.Item>
-    <img width={900} height={500} text-align="center"  alt="900x500" src= { require("./horse.jpg")} />
+    <img width={900} height={500} text-align="center"  alt="900x500" src= { require("../public/horse.jpg")} />
       <Carousel.Caption>
         <h3>马3的名字</h3>
         <p>马3的介绍</p>
