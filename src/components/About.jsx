82d66911bd237867d850d0fcaa8344ec4e7f361d
Weiguo <wxhh61@gmail.com>
2016-06-17 16:32:17 -0400

    add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import { Navbar, Nav , NavItem} from 'react-bootstrap';
 import {Link } from 'react-router'
 
-
 export default React.createClass({
   render() {
     return (
-    <div>
-      <h2>About</h2>
-    </div>
+        <div>
+        <h2>About</h2>
+        </div>
     );
   }
 })
